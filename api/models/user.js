cb1420c280f9738e8b496908bfe05b6fd7df988f
MSI\ajg <alex.j.greff@gmail.com>
2018-12-19 02:04:18 -0500

    Small change to User schema

--- api/models/user.js
+++ api/models/user.js
@@ -10,7 +10,7 @@ const userSchema = mongoose.Schema({
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
     password: { type: String, required: true },
-    role: { type: String, default: 'default' }
+    role: { type: String, default: 'default', required: true }
 });
 
 module.exports = mongoose.model('User', userSchema);
