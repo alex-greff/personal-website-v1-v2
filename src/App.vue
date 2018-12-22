554fca6f1a7023e46ab1ed638cc32beb3104d723
MSI\ajg <alex.j.greff@gmail.com>
2018-12-22 14:32:53 -0500

    Revert "Added routes"
    This reverts commit e3f2910ad6bbb11c8f88905fe00f2d97dcdea1ab.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,8 @@
 <template>
     <div>
-        <nav-bar></nav-bar>
-        <router-view></router-view>
+        
     </div>
 </template>
-
-<script>
-    import NavBar from './components/NavBar.vue';
-    export default {
-        components: {
-            navBar: NavBar
-        }
-    }
-</script>
-
 
 <style lang="scss">
 
--- src/components/Footer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <h1>Footer Component</h1>
-    </div>
-</template>
