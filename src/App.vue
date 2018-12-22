e3f2910ad6bbb11c8f88905fe00f2d97dcdea1ab
MSI\ajg <alex.j.greff@gmail.com>
2018-12-21 23:44:42 -0500

    Added routes

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
     <div>
-        
+        <nav-bar></nav-bar>
+        <router-view></router-view>
     </div>
 </template>
+
+<script>
+    import NavBar from './components/NavBar.vue';
+    export default {
+        components: {
+            navBar: NavBar
+        }
+    }
+</script>
+
 
 <style lang="scss">
 
---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <nav>
+            <router-link to="/" tag="a">Home</router-link>
+            <router-link to="/about" tag="a">About</router-link>
+            <router-link to="/projects" tag="a">Projects</router-link>
+            <router-link to="/experience" tag="a">Experience</router-link>
+            <router-link to="/music" tag="a">Music</router-link>
+            <router-link to="/contact" tag="a">Contact</router-link>
+        </nav>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
