0f2543b045e326cba456ec852d9e18fa491d6163
MSI\ajg <alex.j.greff@gmail.com>
2018-12-22 22:21:08 -0500

    Worked on footer

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
-    <div>
+    <div id="app">
         <nav-bar></nav-bar>
         <router-view></router-view>
+        <general-footer></general-footer>
     </div>
 </template>
 
 <script>
     import NavBar from './components/NavBar.vue';
+    import Footer from './components/Footer.vue';
 
     export default {
         components: {
-            navBar: NavBar
+            navBar: NavBar,
+            generalFooter: Footer
         }
     }
 </script>
 
 
 <style lang="scss">
-
+    
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,53 @@
 <template>
-    <div>
-        <h1>Footer Component</h1>
-    </div>
+    <footer class="footer">
+        <div class="credits">Designed and Built by Alexander Greff</div>
+        <div class="social-media">
+            <i class="fab fa-github icon"></i>
+            <i class="fab fa-linkedin icon"></i>
+            <i class="fas fa-envelope icon"></i>
+            <i class="fab fa-soundcloud icon"></i>
+        </div>
+    </footer>
 </template>
+
+<style lang="scss">
+    #app {
+        min-height: 100%;
+        display: grid;
+        grid-template-rows: 1fr auto;
+    }
+
+    .footer {
+        font-size: 1.2rem;
+
+        text-align: center;
+
+        grid-row-start: 2;
+        grid-row-end: 3;
+
+        padding: 1.3rem;
+        z-index: 5;
+
+        background: rgb(41, 41, 41);
+        color: rgb(151, 151, 151);
+        // box-shadow: 0 -2px 2px rgba(44, 44, 44, 0.445);
+    }
+
+    .social-media {
+        margin-top: 0.5rem;
+
+        & > .icon {
+            font-size: 1.7rem;
+
+            margin-right: 0.5rem;
+            margin-left: 0.5rem;
+
+            cursor: pointer;
+
+            &:hover {
+                color: $nav-color-tertiary;
+            }
+        }
+    }
+</style>
+
