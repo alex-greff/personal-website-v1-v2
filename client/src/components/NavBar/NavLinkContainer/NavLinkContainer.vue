6da49c135401a1bcb5ca4b11edef61485e38d63d
alex-greff <alex.j.greff@gmail.com>
2019-03-15 22:43:56 -0400

    Added animations to navbar

--- client/src/components/NavBar/NavLinkContainer/NavLinkContainer.vue
+++ client/src/components/NavBar/NavLinkContainer/NavLinkContainer.vue
@@ -6,9 +6,13 @@
         <nav-link-item>Something 3</nav-link-item> -->
 
         <nav-link-item 
-            v-for="page in orderedPages" 
+            v-for="page in navLinkPagesData" 
             :key="page.name"
+
+            :style="navLinkStyles"
+
             :to="page.path"
+            ref="navLinkRefs"
         >
             {{ page.name }}
         </nav-link-item>
@@ -17,6 +21,11 @@
 
 <script>
 import NavLinkItem from "@/components/NavBar/NavLinkContainer/NavLinkItem/NavLinkItem.vue";
+
+import { TweenMax } from "gsap/all";
+
+const ANIM_DURATION = 0.3;
+const ANIM_STAGGER = 0.05;
 
 export default {
     components: {
@@ -32,18 +41,69 @@ export default {
             required: true
         }
     },
+    data() {
+        return {
+            displayNavItems: this.isOpen // Initialize to the init isOpen prop
+        }
+    },
+    methods: {
+        _setDisplayNavItems(i_bIsOpen) {
+            this.displayNavItems = i_bIsOpen;
+        },
+        openNavItems() {
+            // Display the nav items
+            this._setDisplayNavItems(true);
+
+            // Stop the close animation if it's running
+            TweenMax.killTweensOf(this.navLinkElsReversed);
+
+            // Run the open animation
+            const ANIM_OPTIONS = { opacity: 1, x: 0 };
+            TweenMax.staggerTo(this.navLinkEls, ANIM_DURATION, ANIM_OPTIONS, ANIM_STAGGER);
+        },
+        closeNavItems() {
+            // Stop the open animation if it's running
+            TweenMax.killTweensOf(this.navLinkEls);
+
+            // Run the close animation
+            const ANIM_OPTIONS = { opacity: 0, x: 20, onComplete: () => {
+                this._setDisplayNavItems(false);
+            }};
+            TweenMax.staggerTo(this.navLinkElsReversed, ANIM_DURATION, ANIM_OPTIONS, ANIM_STAGGER);
+        }
+    }, 
     mounted() {
-        
+        // Initialize the nav item opacities
+        this.navLinkEls.forEach(el => {
+            el.style.opacity = (this.isOpen) ? 1 : 0;
+        });
     },
     computed: {
-        orderedPages() {
+        navLinkPagesData() {
             return this.pages.reverse();
+        },
+        navLinkStyles() {
+            return {
+                // Set the display property based on the state of the nav items
+                display: (this.displayNavItems) ? "initial" : "none",
+            }
+        },
+        navLinkEls() {
+            return this.$refs.navLinkRefs.map(ref => ref.$el);
+        },
+        navLinkElsReversed() {
+            return this.navLinkEls.slice().reverse();
         }
     },
     watch: {
         // Watch when isOpen toggles
-        isOpen(prevIsOpen, nextIsOpen) {
-            // TODO: run the fade-in/fade-out anim
+        isOpen(isOpening, isCurrenlyOpen) {
+            if (isOpening) {
+                this.openNavItems();
+            }
+            else {
+                this.closeNavItems();
+            }
         }
     }
 }
