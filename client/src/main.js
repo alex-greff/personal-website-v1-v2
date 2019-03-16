cd43d392d8cfc2c4a53ac63c9004f1689c1e01bc
alex-greff <alex.j.greff@gmail.com>
2019-03-15 23:53:24 -0400

    Setup vue-fragement in project

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+import VueFragment from "vue-fragment"
 
 import App from './App.vue';
 import router from './router/router';
@@ -14,6 +15,8 @@ Vue.config.productionTip = false;
 // Material UI 
 Vue.use(MdIcon);
 
+// Vue Fragment
+Vue.use(VueFragment.Plugin);
 
 // Bind axios to vue
 // Note: the default request url can be set with axios.defaults.baseURL = <some url>;
