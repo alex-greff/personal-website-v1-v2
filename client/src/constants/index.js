3d3e6300302216e4a0acab95ca341009f5fff49a
alex-greff <alex.j.greff@gmail.com>
2019-03-17 00:39:27 -0400

    Moved out default local theme to constants file. Removed projectItems and themeItems constant files

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,9 +1,10 @@
 import themeItems from './themeItems';
 import projectItems from './projectItems';
 import breakpoints from './breakpoints';
+import defaultTheme from './defaultTheme';
 
 export default {
-    ...themeItems,
     ...projectItems,
-    ...breakpoints
+    ...breakpoints,
+    ...defaultTheme
 };
