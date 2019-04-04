e55897e5b30f365c39da7b295042df26b68fe5a0
alex-greff <alex.j.greff@gmail.com>
2019-04-03 20:05:13 -0400

    Moved old components into _old folder

---
+++ client/src/_old/Projects_old/ProjectListItem.vue
@@ -0,0 +1,215 @@
+<template>
+    <div class="project-list-item">
+        <div class="thumbnail">
+            <div class="image-tint">
+                <div class="thumbnail-image" :style="thumbnailImageStyles"></div>
+            </div>
+        </div>
+        <div class="project-info">
+            <div class="project-info-container">
+                <div class="project-name">
+                    {{ projectData.name }}
+                </div>
+                <div class="project-summary">
+                    {{ projectData.summary }}
+                </div>
+                <div class="project-tags">
+                    <div class="tag-header">Tags:</div>
+                    <div class="tag">
+                        {{ tagStringList }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        projectData: { type: Object, required: true }
+    },
+    data() {
+        return {
+
+        };
+    },
+    computed: {
+        thumbnailImageStyles() {
+            return { backgroundImage: `url('${this.projectData.thumbnailImage}')` };
+        },
+        tagStringList() {
+            let stringList = "";
+            this.projectData.tags.forEach((tag) => stringList += tag + ', ' );
+            return stringList.trim().replace(/,$/, "");
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    $project-item-height: 10rem;
+    $thumbnail-width: 20rem;
+    $project-info-width: 50rem;
+
+    .project-list-item {
+        display: grid;
+
+        grid-template-columns: auto $thumbnail-width 50rem auto;
+        grid-template-rows: $project-item-height;
+
+        grid-column-gap: 2rem;
+
+        @include respond(tab-port) {
+            grid-template-columns: auto $thumbnail-width 35rem auto;
+        }
+
+        @include respond(phone) {
+            grid-template-columns: auto 1fr auto;
+        }
+
+        @include H_Align(relative);
+
+        transition: background-color 0.5s;
+
+        // Hover stylings   
+        &:hover {
+            background-color: rgba(0, 0, 0, 0.075);
+
+            & .image-tint {
+                background-color: theme-link("page", "selected-color", "primary", 0); // TODO: not the right selector name
+            }
+
+            & .thumbnail {
+                clip-path: polygon(0 0%, 100% 0%, 100% 100%, 0 100%);
+
+                & .thumbnail-image {
+                    @include respond(phone) {
+                        filter: grayscale(75%) brightness(50%);
+                    }
+
+                    transform: scale(1);
+                    filter: grayscale(0%) brightness(100%);
+                    opacity: 1;
+                }
+            }
+
+            & .project-info > .project-name {
+                color: theme-link("page", "accent-color", "primary");
+            }
+        }
+
+        & .image-tint {
+            @include respond(phone) {
+                background-color: theme-link("page", "selected-color", "primary", 0.6); // TODO: not the right selector name
+            };
+
+            background-color: theme-link("page", "selected-color", "primary"); // TODO: not the right selector name
+
+            transition: background-color 0.5s;
+
+            z-index: 2;
+        }
+
+        & .thumbnail {
+            // Note: Needed to stop flickering of project info items
+            will-change: clip-path;
+
+            overflow: hidden;
+
+            transition: clip-path 0.5s;
+
+            clip-path: polygon(20% 0, 100% 0, 80% 100%, 0% 100%);
+
+            width: $thumbnail-width;
+            height: $project-item-height;
+
+            grid-column: 2 / 3;
+            grid-row: 1 / 2;
+
+            @include respond(phone) {
+                grid-column: 2 / 3;
+                grid-row: 1 / 2;
+
+                justify-self: right;
+            }
+
+            & .thumbnail-image {
+                // Note: Needed to stop flickering of project info items
+                will-change: filter, transform, opacity;
+
+                width: $thumbnail-width;
+                height: $project-item-height;
+
+                background-size: cover;
+                background-position: center center;
+
+                transition: filter 0.5s, transform 0.5s, opacity 0.5s;
+
+                transform: scale(1.1);
+                opacity: 0.5;
+                filter: grayscale(100%) brightness(50%);
+
+                z-index: 1;
+            }
+        }
+
+        & .project-info {
+            grid-column: 3 / 4;
+            grid-row: 1 / 2;
+
+            @include respond(phone) {
+                grid-column: 2 / 3;
+                grid-row: 1 / 2;
+            }
+
+            // max-width: 50rem;
+
+            z-index: 3;
+
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+
+            & > .project-info-container {
+                // TODO: is this wanted?
+                // @include respond(phone) {
+                //     background-color: rgba(0, 0, 0, 0.2); // TODO: theme
+                // }
+
+                padding: 0.5rem;
+
+                & > *:not(:last-child) {
+                    padding-bottom: .4rem;
+                }
+                
+
+                & > .project-name {
+                    font-size: 1.7rem;
+                    font-weight: 500;
+
+                    color: theme-link("page", "accent-color", "primary");
+
+                    transition: color 0.5s;
+                }
+
+                & > .project-summary {
+                    font-weight: 300;
+                }
+
+                & > .project-tags {
+                    font-weight: 300;
+
+                    & > .tag-header {
+                        font-weight: 400;
+                        color: theme-link("page", "selected-color", "primary"); // TODO: not the right selector name
+                    }
+
+                    & > * {
+                        display: inline;
+                    }
+                }
+            }
+        }
+    }
+</style>
---
+++ client/src/_old/Projects_old/Projects.vue
@@ -0,0 +1,123 @@
+<template>
+    <div class="projects-content">
+        <div class="projects-nav-page" v-if="!isSubPage">
+            <h1>Projects</h1>
+
+            <!-- TODO: old stuff -->
+            <ul class="projects-list" v-if="projectDataLoaded">
+                <router-link
+                    v-for="project in projects"
+                    :key="project.name"
+                    :to="`/projects/${project.name}`"
+                    tag="li"
+                    class="project-link"
+                >
+                    <project-list-item :projectData="project" />
+                </router-link>
+            </ul>
+            <div v-else>
+                Loading...
+            </div>
+
+            <!-- <content-list
+                :contentIDs="projectIDs"
+            >
+                <template v-for="(val, key, index) in projects" v-slot:[key]>
+                    <h1 :key="index">{{key}}</h1>
+                </template>
+
+            </content-list> -->
+
+        </div>
+        
+        <!-- Subpage router view -->
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import ProjectListItem from './ProjectListItem.vue';
+import { getterTypes } from '@/store/types';
+
+import ContentList from '../../components/contentList/ContentList.vue';
+
+export default {
+    components: {
+        projectListItem: ProjectListItem,
+        contentList: ContentList,
+    },
+    computed: {
+        ...mapGetters({
+            projects: getterTypes.GET_ALL_PROJECTS
+        }),
+        isSubPage() {
+            return !!(this.$route.params.id);
+        },
+        projectDataLoaded() {
+            return !!(this.projects);
+        },
+        projectIDs() {
+            return Object.entries(this.projects).map(([projectName]) => projectName);
+        }
+    },
+    // ------------------
+    // --- Animations ---
+    // ------------------
+    enterAnim(el) {
+        return new Promise((resolve, reject) => {
+            console.log("Running Projects enter anim"); 
+            // TODO: animate here
+            setTimeout(() => resolve(), 0); // TODO: remove
+        });
+    },
+    leaveAnim(el) {
+        return new Promise((resolve, reject) => {
+            console.log("Running Projects leave anim"); 
+            // TODO: animate here
+            setTimeout(() => resolve(), 100); // TODO: remove
+        });
+    }
+}
+</script>
+
+<style lang="scss" scoped>    
+    $grid-padding: 2.5rem;
+
+    .projects-content {
+
+    }
+
+    .projects-nav-page {
+        position: relative;
+        
+        margin: 5rem 0 0 0;
+
+        & > h1 {
+            text-align: center;
+
+            font-size: 3rem;
+            color: theme-link("page", "accent-color", "primary");
+        }
+
+        & > .projects-list {
+            list-style-type: none;
+
+            margin-top: 2rem;
+
+            & > .project-link {
+                text-decoration: none;
+                cursor: pointer;
+
+                & > * {
+                    margin-bottom: 2rem;
+                }
+
+                &:last-child {
+                    margin-bottom: 5rem;
+                }
+            }
+        }
+    }
+
+</style>
