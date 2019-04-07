af08544b0b987f518e1bd9ef6a1af4e566479bb4
alex-greff <alex.j.greff@gmail.com>
2019-04-07 14:19:54 -0400

    Added font-awesome integration and imported needed icons

--- client/src/main.js
+++ client/src/main.js
@@ -10,10 +10,29 @@ import store from './store/store';
 import { MdIcon } from 'vue-material/dist/components';
 import 'vue-material/dist/vue-material.min.css';
 
+import Icon from 'vue-awesome/components/Icon';
+import "vue-awesome/icons/brands/github";
+import "vue-awesome/icons/brands/linkedin";
+import "vue-awesome/icons/envelope";
+import 'vue-awesome/icons/brands/soundcloud';
+import "vue-awesome/icons/external-link-alt";
+import "vue-awesome/icons/brands/facebook";
+import "vue-awesome/icons/brands/instagram";
+import "vue-awesome/icons/brands/pinterest";
+import "vue-awesome/icons/brands/snapchat";
+import "vue-awesome/icons/brands/youtube";
+import "vue-awesome/icons/brands/vimeo-v";
+import "vue-awesome/icons/brands/docker";
+import "vue-awesome/icons/brands/twitter";
+import "vue-awesome/icons/link";
+
 Vue.config.productionTip = false;
 
 // Material UI 
 Vue.use(MdIcon);
+
+// Font Awesome
+Vue.component('fa-icon', Icon);
 
 // Vue Fragment
 Vue.use(VueFragment.Plugin);
