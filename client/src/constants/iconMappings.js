fffb5cadc28d04944fc3ec1ef53628bee269f11e
alex-greff <alex.j.greff@gmail.com>
2019-04-07 14:20:20 -0400

    Added iconMappings constant file

---
+++ client/src/constants/iconMappings.js
@@ -0,0 +1,16 @@
+export default {
+    "github": "brands/github",
+    "linkedin": "brands/linkedin",
+    "email": "envelope",
+    "soundcloud": "brands/soundcloud",
+    "website": "external-link-alt",
+    "facebook": "brands/facebook",
+    "instagram": "brands/instagram",
+    "pinterest": "brands/pinterest",
+    "snapchat": "brands/snapchat",
+    "youtube": "brands/youtube",
+    "vimeo": "brands/vimeo-v",
+    "docker": "brands/docker",
+    "twitter": "brands/twitter",
+    "default": "link",
+};
--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,7 +1,9 @@
 import breakpoints from './breakpoints';
 import pageData from './pageData';
+import iconMappings from './iconMappings';
 
 export default {
     ...breakpoints,
-    ...pageData
+    ...pageData,
+    iconMappings,
 };
