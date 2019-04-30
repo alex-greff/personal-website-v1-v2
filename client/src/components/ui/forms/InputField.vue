ecc5ea82519eac53ac8f0eb9650c72d045faa2d0
alex-greff <alex.j.greff@gmail.com>
2019-04-29 22:30:21 -0400

    Added base code for InputField

---
+++ client/src/components/ui/forms/InputField.vue
@@ -0,0 +1,44 @@
+<template>
+    <input 
+        class="InputField"
+        :type="type"
+        :value="value"
+        @input="handleInput"
+    >
+</template>
+
+<script>
+export default {
+    props: {
+        name: {
+            type: String,
+            default: ""
+            // required: true
+        },
+        type: {
+            type: String,
+            default: "text",
+            validator(val) {
+                const validTypes = ["button", "checkbox", "color", 
+                                    "date", "datetime-local", "email", "file", "hidden", "image",
+                                    "month", "number", "password", "radio", "range", "search", 
+                                    "submit", "tel", "text", "url", "week"];
+                return validTypes.indexOf(val) > -1;
+            }
+        },
+        value: {
+            type: String,
+            default: ""
+        }
+    },
+    methods: {
+        handleInput(e) {
+            this.$emit("input", e.target.value);
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- client/src/views/Contact.vue
+++ client/src/views/Contact.vue
@@ -1,11 +1,38 @@
 <template>
-    <div class="contact-content">
-        <h1>Contact Page Component</h1>
+    <div class="Contact">
+        <div class="Contact__content">
+            <h1 class="Contact__title">
+                Contact
+            </h1>
+            <form 
+                class="Contact__form"
+                @submit.prevent="submit"
+            >
+                <input-field v-model="someVal" />
+
+                <button type="submit">Send</button>
+            </form>
+        </div>
     </div>
 </template>
 
 <script>
+import InputField from "@/components/ui/forms/InputField.vue";
+
 export default {
+    components: {
+        inputField: InputField,
+    },
+    data() {
+        return {
+            someVal: "hi"
+        }
+    },
+    methods: {
+        submit() {
+            console.log("Submitting form");
+        }
+    },
     // ------------------
     // --- Animations ---
     // ------------------
@@ -25,3 +52,50 @@ export default {
     }
 }
 </script>
+
+<style lang="scss" scoped>
+    .Contact {
+        & .Contact__content {
+            position: relative;
+        
+            margin-top: 4.5rem;
+
+            & .Contact__title {
+                text-align: center;
+                font-weight: 600; // h1 sets this by default
+
+                font-size: 4rem;
+                line-height: 4rem;
+                color: theme-link("page", "accent-color", "primary");
+            }
+
+            // ---------------------
+            // --- Media Queries ---
+            // ---------------------
+            @include respond(big-desktop) {
+                margin-right: 20rem;
+                margin-left: 20rem;
+            }
+
+            @include respond(normal) {
+                margin-right: 15rem;
+                margin-left: 15rem;
+            }
+
+            @include respond(tab-land) {
+                margin-right: 10rem;
+                margin-left: 10rem;
+            }
+
+            @include respond(tab-port) {
+                margin-right: 5rem;
+                margin-left: 5rem;
+            }
+
+            @include respond(phone) {
+                margin-right: 3rem;
+                margin-left: 3rem;
+            }
+        }
+    }
+</style>
