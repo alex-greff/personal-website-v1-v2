806bf1bc8751ea9eee310911c7723ba5793a5145
alex-greff <alex.j.greff@gmail.com>
2019-05-04 16:59:18 -0400

    Small code changes

--- client/src/App.vue
+++ client/src/App.vue
@@ -269,13 +269,4 @@ export default {
 
         pointer-events: none;
     }
-
-    // TODO: remove
-    .component-fade-enter-active, .component-fade-leave-active {
-        transition: opacity 3s ease;
-    }
-    .component-fade-enter, .component-fade-leave-to
-        /* .component-fade-leave-active below version 2.1.8 */ {
-        opacity: 0;
-    }
 </style>
