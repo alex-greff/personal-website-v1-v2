38bc4937deb5411a1d5ee0fd04fb8d2647613998
alex-greff <alex.j.greff@gmail.com>
2019-05-19 16:33:49 -0400

    Cleaned up config

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,34 +1,16 @@
-// var pathToBourbon = require('node-bourbon').includePaths;
-
 module.exports = {
     devServer: {
         disableHostCheck: true,
         watchOptions: {
             poll: true
         },
-        // Some stuff that isn't needed at the moment
-        // public: '0.0.0.0:8080'
-        // host: '0.0.0.0',
-        // port: 8080,
-        // proxy: {
-        //     "/api/*": {
-        //         target: "http://localhost:5000",
-        //         secure: false
-        //     },
-        //     "/uploads/*": {
-        //         target: "http://localhost:5000",
-        //         secure: false
-        //     }
-        // }
     },
     css: {
         loaderOptions: {
             sass: {
                 data: `
                     @import "@/styling/main.scss";
-                    @import "./node_modules/bourbon/app/assets/stylesheets/_bourbon.scss"; // TODO: this is whack... fix later maybe
                 `,
-                // includePaths: [pathToBourbon]
             }
         }
     },
