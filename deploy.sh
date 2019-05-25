539f22b5b68dc7422419513509322011b4f12a3b
alex-greff <alex.j.greff@gmail.com>
2019-05-25 18:07:26 -0400

    Cleaned up scripts. Quoted all ssh env var exports just to be safe

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,4 @@ set -xe # -x print commands, -e exit immediately if non-zero exit code occurs
 
 cd /var/app
 
-
-echo "SHA $SHA"
-echo "MONGO URI $PERSONAL_WEBSITE_MONGO_URI"
-echo "MONGO USERNAME $PERSONAL_WEBSITE_MONGO_USERNAME"
-echo "MONGO PASS $PERSONAL_WEBSITE_MONGO_PASSWORD"
-echo "JWT SECRET $PERSONAL_WEBSITE_JWT_SECRET"
-echo "JWT EXPIRE $PERSONAL_WEBSITE_JWT_TOKEN_EXPIRE_TIME"
-
 echo "$TRAVIS_USER_PASSWORD" | sudo -SE docker-compose up -d --force-recreate
