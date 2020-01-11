3639e0b2108d325f39c4633db78172fa08008c82
alex-greff <alex.j.greff@gmail.com>
2020-01-10 21:34:27 -0500

    Moved out some constants to the general constants file

---
+++ client/src/constants/general.js
@@ -0,0 +1,2 @@
+export const GLOBAL_NAMESPACE = "global";
+export const GLOBAL_THEME = "default";
--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,9 +1,11 @@
 import breakpoints from './breakpoints';
 import pageData from './pageData';
 import iconMappings from './iconMappings';
+import general from "./general";
 
 export default {
     ...breakpoints,
     ...pageData,
+    ...general,
     iconMappings,
 };
